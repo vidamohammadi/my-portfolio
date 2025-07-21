4a30601fbdbd720a1956feb572efdd9e2d43c4c9
vida <m.vidamohammadi@gmail.com>
2025-07-20 17:48:59 -0800

    init portfolio with Nuxt & Vuetify

---
+++ plugins/vuetify.js
@@ -0,0 +1,13 @@
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+export default defineNuxtPlugin(app => {
+    const vuetify = createVuetify({
+        components,
+        directives,
+        ssr: true
+    })
+
+    app.vueApp.use(vuetify)
+})
