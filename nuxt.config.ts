bd6396d672fc02e2e4e4a046deb268c77b6341aa
vida <m.vidamohammadi@gmail.com>
2025-08-05 13:49:40 -0800

    fix: configure baseURL for GitHub Pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-    ssr: true,
-    build: {
-        transpile: ['vuetify']
-    },
-    css: ['vuetify/styles'],
-    plugins: ['/plugins/vuetify.ts']
-
-})
+  devtools: {enabled: true},
+  ssr: false,
+  target: 'static',
+  app: {
+    baseURL: '/my-portfolio/'
+  },
+  build: {
+    transpile: ['vuetify']
+  },
+  css: ['vuetify/styles'],
+  plugins: ['/plugins/vuetify.ts']
+});
