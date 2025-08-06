d7951653f91d84ac39a6b0a44aa9173350b66517
vida <m.vidamohammadi@gmail.com>
2025-08-05 20:58:16 -0800

    fix: add mobile design for introduction page

--- components/introduction/index.vue
+++ components/introduction/index.vue
@@ -1,4 +1,8 @@
 <script setup>
+  import {useDisplay} from 'vuetify';
+
+  const {lgAndUp} = useDisplay();
+  const isDesktop = computed(() => lgAndUp.value);
   const downloadCV = () => {
     const contractPath = '/my-portfolio/documents/Resume.pdf';
     const link = document.createElement('a');
@@ -10,11 +14,11 @@
 <template>
   <v-container class="mt-10">
     <v-row>
-      <v-col cols="5">
+      <v-col v-if="isDesktop" cols="5">
         <v-img src="/my-portfolio/images/IMG_E3746.JPG" height="500px" />
       </v-col>
-      <v-col cols="7">
-        <div class="ml-8 mt-5">
+      <v-col cols="12" lg="7">
+        <div class="ml-lg-8 mt-5 text-lg-body-1 text-caption">
           <p class="text-primary mb-3">Who am i?</p>
           <h1 class="font-weight-bold mb-3">
             I'm Vida Mohammadi, a FrontEnd developer
@@ -27,8 +31,8 @@
             Implemented advanced state management using Vuex and Pinia to
             optimize API data flow and performance.
           </p>
-          <v-divider class="border-opacity-100 my-10" />
-          <v-row>
+          <v-divider v-if="isDesktop" class="border-opacity-100 my-10" />
+          <v-row v-if="isDesktop">
             <v-col cols="6">
               <p>Name: Vida Mohammadi</p>
             </v-col>
@@ -36,7 +40,7 @@
               <p>Location: Tehran, Iran</p>
             </v-col>
           </v-row>
-          <v-row>
+          <v-row v-if="isDesktop">
             <v-col cols="6">
               <p>age: 28</p>
             </v-col>
@@ -44,8 +48,10 @@
               <p>Email: m.vidamohammadi@gmail.com</p>
             </v-col>
           </v-row>
-          <v-row>
-            <v-btn class="bg-primary text-body-2 rounded-md mt-5 text-white" @click='downloadCV'>
+          <v-row :class="isDesktop ? '' : 'w-100 d-flex justify-end '">
+            <v-btn
+              class="bg-primary rounded-md mt-lg-5 mt-10 text-white text-lg-body-2 text-caption"
+              @click="downloadCV">
               Download CV
             </v-btn>
           </v-row>
